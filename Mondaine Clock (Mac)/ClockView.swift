c3c18fd638d5603f28d286cd9d70e83b0769af2a
WoodenAxe <473072010@qq.com>
2024-08-11 17:45:00 +0800

    Refactor ClockView to remove colorScheme checks and use asset catalog for dark/light mode images

--- Mondaine Clock (Mac)/ClockView.swift
+++ Mondaine Clock (Mac)/ClockView.swift
@@ -1,57 +1,48 @@
-//
-//  ContentView.swift
-//  Mondaine Clock (Mac)
-//
-//  Created by Steve Wang on 8/10/24.
-//
-
-
 import SwiftUI
 
 struct ClockView: View {
-    @Environment(\.colorScheme) var colorScheme // 检测系统主题
     @State private var currentTime = CurrentTime()
     @State private var lastSecond: Int = 0
 
     var body: some View {
         ZStack {
-            // 背景图像
-            Image(colorScheme == .dark ? "BG_Dark" : "BG")
+            // Background image
+            Image("BG")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
 
-            // 表盘
-            Image(colorScheme == .dark ? "ClockFace_Dark" : "ClockFace")
+            // Clock Face
+            Image("ClockFace")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 785, height: 785) // 固定表盘的大小
+                .frame(width: 785, height: 785) // face size
                 .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 5)
 
-            // 刻度
-            Image(colorScheme == .dark ? "ClockIndicator_Dark" : "ClockIndicator")
+            // Indicator
+            Image("ClockIndicator")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 730, height: 730)
-                .offset(y: 4) // 修改 offset 使其居中
+                .offset(y: 4) // make it center
                 .shadow(color: .black.opacity(0.5), radius: 2, x: 0, y: 2)
 
-            // 时针
-            Image(colorScheme == .dark ? "HOURBAR_Dark" : "HOURBAR")
+            // Hour hand
+            Image("HOURBAR")
                 .resizable()
                 .frame(width: 50, height: 433.87)
                 .rotationEffect(Angle.degrees(currentTime.hoursAngle))
                 .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 5)
 
-            // 分针
-            Image(colorScheme == .dark ? "MINBAR_Dark" : "MINBAR")
+            // Minute hand
+            Image("MINBAR")
                 .resizable()
                 .frame(width: 50, height: 685.73)
                 .rotationEffect(Angle.degrees(currentTime.minutesAngle))
                 .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 5)
 
-            // 秒针
-            Image(colorScheme == .dark ? "REDINDICATOR_Dark" : "REDINDICATOR")
+            // Second hand
+            Image("REDINDICATOR")
                 .resizable()
                 .frame(width: 383, height: 579)
                 .offset(y: -1)
